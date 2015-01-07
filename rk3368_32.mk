44189e12bd248e90cbd74adb03119fcd99d8e757
shen zhi cheng <szc@rock-chips.com>
2015-01-07 11:33:25 +0800

    PRODUCT_NAME := rk3368_32 PRODUCT_DEVICE := rk3368

--- rk3368_32.mk
+++ rk3368_32.mk
@@ -16,6 +16,6 @@
 
 $(call inherit-product, $(LOCAL_PATH)/rk3368.mk)
 
-PRODUCT_NAME := rk3368
-PRODUCT_DEVICE := rk3368_32
+PRODUCT_NAME := rk3368_32
+PRODUCT_DEVICE := rk3368
 PRODUCT_MODEL := 32-bit AOSP on rk3368
